9c2ef1b5f441d1035aef9b5940d1384726566d7a
DuctileBonsai49 <alexgaming.padh@gmail.com>
2025-05-18 21:38:38 +0200

    script.js aktualisieren
    Fehler

--- script.js
+++ script.js
@@ -15,14 +15,47 @@ const loesungen = {
       <li>Netz manuell auswählen</li>
     </ul>
   `,
+  "SIMon mobile|SIM wird nicht erkannt": `
+    <ul>
+      <li>SIM neu einsetzen</li>
+      <li>Handy neu starten</li>
+      <li>SIM in anderem Gerät testen</li>
+    </ul>
+  `,
+  "SIMon mobile|Mobile Daten zu langsam": `
+    <ul>
+      <li>Netzmodus auf 4G/LTE stellen</li>
+      <li>Datenvolumen prüfen</li>
+    </ul>
+  `,
   "Telekom|Kein Empfang": `
     <ul>
       <li>Flugmodus deaktivieren und wieder aktivieren</li>
       <li>Netz manuell wählen</li>
-      <li>Gerät neustarten</li>
+      <li>Gerät neu starten</li>
     </ul>
-  `
-  // Weitere Kombinationen kannst du hier ergänzen
+  `,
+  "Telekom|Roaming funktioniert nicht": `
+    <ul>
+      <li>Im Kundencenter prüfen, ob Roaming aktiviert ist</li>
+      <li>Netz manuell wählen</li>
+      <li>APN-Einstellungen überprüfen</li>
+    </ul>
+  `,
+  "Telekom|SIM wird nicht erkannt": `
+    <ul>
+      <li>SIM neu einsetzen</li>
+      <li>Gerät neu starten</li>
+      <li>SIM in anderem Gerät testen</li>
+    </ul>
+  `,
+  "Telekom|Mobile Daten zu langsam": `
+    <ul>
+      <li>Netzmodus auf LTE einstellen</li>
+      <li>Datenverbrauch prüfen</li>
+    </ul>
+  `,
+  // Weitere Anbieter & Probleme können hier ergänzt werden
 };
 
 document.getElementById("zeigeBtn").addEventListener("click", () => {
@@ -35,13 +68,5 @@ document.getElementById("zeigeBtn").addEventListener("click", () => {
     container.innerHTML = `<h3>Lösung für ${anbieter} – ${problem}</h3>${loesungen[key]}`;
   } else {
     container.innerHTML = `<p>Für diese Kombination liegt kein Lösungsvorschlag vor.</p>`;
-  }
-});
-
-document.getElementById("supportBtn").addEventListener("click", function () {
-  if (window.JFChatAgent) {
-    window.JFChatAgent.open();
-  } else {
-    alert("Support-Chat wird geladen. Bitte kurz warten und erneut versuchen.");
   }
 });
